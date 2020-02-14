77329c00ddd81ccdd7c7e11251af30031649c09c
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-02-14 15:34:31 +0000

    Updates

---
+++ precache-manifest.1e36f7693e4902f702988156e0c6cec0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b124e6f98119aad102cba2dcf540977",
+    "url": "/glasswall-file-type-detection/index.html"
+  },
+  {
+    "revision": "35f5afdbd739fc2f5f5d",
+    "url": "/glasswall-file-type-detection/static/css/2.fddaa3a5.chunk.css"
+  },
+  {
+    "revision": "ea07473d1e2ff65ccaf5",
+    "url": "/glasswall-file-type-detection/static/css/main.209ea1be.chunk.css"
+  },
+  {
+    "revision": "35f5afdbd739fc2f5f5d",
+    "url": "/glasswall-file-type-detection/static/js/2.c76f22a7.chunk.js"
+  },
+  {
+    "revision": "ea07473d1e2ff65ccaf5",
+    "url": "/glasswall-file-type-detection/static/js/main.45206c7a.chunk.js"
+  },
+  {
+    "revision": "7c2424e46c3b48afcdb8",
+    "url": "/glasswall-file-type-detection/static/js/runtime-main.d67c28e5.js"
+  },
+  {
+    "revision": "8503c64fa486375dc20b3c00aa2e250a",
+    "url": "/glasswall-file-type-detection/static/media/download-icon-white.8503c64f.svg"
+  },
+  {
+    "revision": "6332dd8cb99e02f089de4f747566362c",
+    "url": "/glasswall-file-type-detection/static/media/drop-grey.6332dd8c.svg"
+  },
+  {
+    "revision": "813b1258bed1257b91bcff7bf0282fbc",
+    "url": "/glasswall-file-type-detection/static/media/drop-white.813b1258.svg"
+  },
+  {
+    "revision": "34536746f7b8c5b46d8675f1941e1dbc",
+    "url": "/glasswall-file-type-detection/static/media/info-icon.34536746.svg"
+  },
+  {
+    "revision": "6c8e57277f41d456ad53ae6fcbc74b80",
+    "url": "/glasswall-file-type-detection/static/media/logo.6c8e5727.svg"
+  },
+  {
+    "revision": "d41c631875217e983fe2051e2c62932e",
+    "url": "/glasswall-file-type-detection/static/media/modal-close.d41c6318.svg"
+  },
+  {
+    "revision": "b01fa7f07a4c2e355eff2be5069f4c93",
+    "url": "/glasswall-file-type-detection/static/media/montserrat-regular-webfont.b01fa7f0.woff2"
+  },
+  {
+    "revision": "e8b7d8acf640a69f838ca38c063e33ff",
+    "url": "/glasswall-file-type-detection/static/media/montserrat-regular-webfont.e8b7d8ac.woff"
+  },
+  {
+    "revision": "9d9e09d7ae09f64a8a365e669105bedd",
+    "url": "/glasswall-file-type-detection/static/media/roboto-regular-webfont.9d9e09d7.woff"
+  },
+  {
+    "revision": "d96d07c1e0f7cdf7a1988dcb0bf6e0e4",
+    "url": "/glasswall-file-type-detection/static/media/roboto-regular-webfont.d96d07c1.woff2"
+  }
+]);
